6d4ff1e2bc111b67ab0ed9284ce6b07de9d459e6
Greg Meyer <gregory.meyer@datadoghq.com>
2017-01-13 14:25:05 -0500

    makes pylint faster (#34)

--- lib/tasks/ci/default.rb
+++ lib/tasks/ci/default.rb
@@ -30,8 +30,8 @@ namespace :ci do
         check_env
         sh %(flake8 #{ENV['SDK_HOME']})
         sh %(find #{ENV['SDK_HOME']} -name '*.py' -not\
-               \\( -path '*.cache*' -or -path '*embedded*' -or -path '*venv*' -or -path '*.git*' \\)\
-               | xargs -n 1 pylint --rcfile=#{ENV['SDK_HOME']}/.pylintrc)
+               \\( -path '*.cache*' -or -path '*embedded*' -or -path '*dd-agent*' -or -path '*venv*' -or -path '*.git*' \\)\
+               | xargs -n 80 -P 8 pylint --rcfile=#{ENV['SDK_HOME']}/.pylintrc)
       end
     end
 
