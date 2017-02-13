6663ec781ab9525b051cb21eb852afe8b1118a45
Jaime Fullaondo <truthbk@gmail.com>
2017-02-13 14:50:06 -0500

    [skeleton] adding changelog to the integrations skeleton. (#46)

---
+++ lib/config/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - Skeleton
+
+0.1.0/ Unreleased
+==================
+
+### Changes
+
+* [FEATURE] adds skeleton integration.
--- lib/tasks/ci/common.rb
+++ lib/tasks/ci/common.rb
@@ -175,6 +175,7 @@ def generate_skeleton(integration)
   copy_skeleton('lib/config/metadata.csv', 'metadata.csv', integration)
   copy_skeleton('lib/config/requirements.txt', 'requirements.txt', integration)
   copy_skeleton('lib/config/README.md', 'README.md', integration)
+  copy_skeleton('lib/config/CHANGELOG.md', 'CHANGELOG.md', integration)
   copy_skeleton('lib/config/conf.yaml.example', 'conf.yaml.example', integration)
 end
 
