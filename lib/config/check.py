d0870b32eee00b47246927ae26f59cbb78664f2d
Remi Hakim <remi@datadoghq.com>
2017-03-10 16:19:21 -0500

    [check.py skeleton] No need to have the constructor

--- lib/config/check.py
+++ lib/config/check.py
@@ -14,8 +14,5 @@ EVENT_TYPE = SOURCE_TYPE_NAME = 'skeleton'
 
 class SkeletonCheck(AgentCheck):
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
-
     def check(self, instance):
         pass
